733808abb45227a2bbcb7e386102cbb2885509f8
kn7767 <112599359+kn7767@users.noreply.github.com>
2022-10-03 17:47:15 -0700

    still need to figure out random commas in generator

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # kaylees-module-3
 module 3 - make random password generator 
+
+## User Story
+AS AN employee with access to sensitive data
+I WANT to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security
+
+## Acceptance Criteria
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+
+REF CODE 
+ var el_down = document.getElementById("geeks");
+  
+        /* Function to generate combination of password */
+        function generateP() {
+            var pass = '';
+            var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + 
+                    'abcdefghijklmnopqrstuvwxyz0123456789@#$';
+              
+            for (let i = 1; i <= 8; i++) {
+                var char = Math.floor(Math.random()
+                            * str.length + 1);
+                  
+                pass += str.charAt(char)
+            }
+              
+            return pass;
+        }
+  
+        function gfg_Run() {
+            el_down.innerHTML = generateP();
+        }
+
+
+        function generatePassword() {
+    var length = 8,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+}
--- script.js
+++ script.js
@@ -1,17 +1,63 @@
-// Assignment code here
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+var passIncludes = "";
+// [ ] for arrays 
+var number = ["0","1","2","3","4","5","6","7","8","9"]; 
+var symbol = ["!","@","#","$","%","^","&","*","?"];
+var uppercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var lowercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+function generatePassword() {
+    var passLength = window.prompt("How many characters do you need in your password? Enter a number between 8-128.");
+    if (isNaN(passLength)) {
+      window.alert("Pick a number between 8-128.");
+      return;
+    } 
+    else if (passLength < 8 || passLength > 128) {
+      window.alert("Pick a value between 8-128.")
+      return;
+    } 
+    
+    window.alert("Your password will have " + passLength + " characters.");
+
+    var passIncludes = '';
+
+      console.log(passIncludes)
+    if(confirm("Click OK to include numbers in your password.")) {
+      passIncludes = passIncludes.concat(number);
+    }
+    if(confirm("Click OK to include symbols in your password.")) {
+        passIncludes = passIncludes.concat(symbol);
+    }    
+    if(confirm("Click OK to include lowercase letters in your password.")) {
+        passIncludes = passIncludes.concat(lowercase);
+    }    
+    if(confirm("Click OK to include uppercase letters in your password.")) {
+        passIncludes = passIncludes.concat(uppercase);
+    }
+
+    var randomPass = "";
+    for (var i = 0; i <passLength; i++) { 
+        randomPass = randomPass + passIncludes[Math.floor(Math.random() * passIncludes.length)];
+    }
+    return randomPass;
+}
+
+
+//Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  if (!password) {
+    return;
+  }
 
+  if (password) {
+  passwordText.value = password;
+  }
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
